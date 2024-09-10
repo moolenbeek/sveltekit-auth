964e2e73f6aa56d0c7b9e72a935b3325c3cfdb1c
moolenbeek <emoolenbeek@gmail.com>
2024-09-10 18:29:44 -0400

    add routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import "@picocss/pico";
+	import type { PageData } from "./$types";
+	export let data: PageData;
+	console.log(data);
+</script>
+
+<div class="container">
+	<nav>
+		<ul>
+			<li>
+				<a href="/"> Website </a>
+			</li>
+		</ul>
+		<ul>
+			<form method="POST">
+				<li><a href="/">Home</a></li>
+				<li><a href="/login">Login</a></li>
+				<li><a href="/register">Register</a></li>
+			</form>
+		</ul>
+	</nav>
+</div>
+<slot />
